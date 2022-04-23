f011799d336209ff96f558a124556eae17376aa5
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-04-22 23:16:43 -0300

    Add files via upload

--- graficos_qplot.R
+++ graficos_qplot.R
@@ -21,10 +21,15 @@ iris
 
 ### Uma dimensão
 
-qplot(x = Petal.Length, data = iris)
+qplot(x = Petal.Length, data = iris) # Padrão gera geometria de histograma.
 qplot(x = Petal.Length, data = iris, geom = "histogram") # Produz mesmo gráfico.
 qplot(x = Petal.Length, data = iris, binwidth = 0.1) # Considera as observações como 0.1, espaço entre uma observação e outra.
 qplot(x = Petal.Length, data = iris, geom = "bar") # Frequência de cada valor, no histograma é uma faixa de valores.
 qplot(x = Species, data = iris) # Como o x não é numérico, o padrão gera um gráfico de barras.
 
 ### Duas dimensões
+
+qplot(x = Petal.Length, y = Sepal.Length, data = iris) # Padrão gera geometria de pontos.
+qplot(x = Petal.Length, y = Sepal.Length, data = iris, color = Species)
+qplot(x = Petal.Length, y = Sepal.Length, data = iris, shape = Species)
+qplot(x = Petal.Length, y = Sepal.Length, data = iris, color = Species, shape = Species)
